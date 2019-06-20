9f4b93d17580a23d12a66312614e68344769679b
Osumgba Chiamaka <osumgbachiamaka@gmail.com>
2019-06-20 18:32:19 +0100

    style: formatted code to suit pycodestyle format

--- app.py
+++ app.py
@@ -3,13 +3,15 @@ from database import get_views, get_authors, error_percentage
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-   most_viewers = get_views()
-   authors = get_authors()
-   percentage = error_percentage()
-   return render_template('index.html', results = most_viewers, authors= authors, percentage = percentage)
+    most_viewers = get_views()
+    authors = get_authors()
+    percentage = error_percentage()
+    return render_template('index.html', results=most_viewers,
+        '\n' authors=authors, percentage=percentage)
 
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=8000)
+    app.run(host='0.0.0.0', port=8000)
--- database.py
+++ database.py
@@ -5,21 +5,29 @@ from datetime import datetime
 
 DBNAME = "news"
 
+
 def get_views():
     db = psycopg2.connect(database=DBNAME)
     c = db.cursor()
-    c.execute("select title, count(*) as views from log join articles on articles.slug = split_part(log.path, '/', 3) group by title order by views desc limit 3;")
+    c.execute("""select title, count(*) as views from log
+        join articles on articles.slug = split_part(log.path, '/', 3)
+        group by title order by views desc limit 3;""")
     views = c.fetchall()
     db.close()
     return views
 
+
 def get_authors():
     db = psycopg2.connect(database=DBNAME)
     c = db.cursor()
-    c.execute("select name, count(*) as views from log join articles on articles.slug = split_part(log.path, '/', 3) join authors on articles.author = authors.id group by name order by views desc; ")
+    c.execute("""select name, count(*) as views from log
+        join articles on articles.slug = split_part(log.path, '/', 3)
+        join authors on articles.author = authors.id
+        group by name order by views desc; """)
     authors = c.fetchall()
     db.close()
     return authors
+
 
 def error_percentage():
     db = psycopg2.connect(database=DBNAME)
@@ -27,5 +35,4 @@ def error_percentage():
     c.execute("select * from main where error_percentage > 1.0;")
     error = c.fetchall()
     db.close
-    
     return error
